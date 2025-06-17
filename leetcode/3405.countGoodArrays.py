577b224515130cf66ef14db014ed1dc5d67bd017
あやめさいこう <sean42593919@gmail.com>
2025-06-18 02:11:25 +0800

    Completed countGoodArrays

--- leetcode/3405.countGoodArrays.py
+++ leetcode/3405.countGoodArrays.py
@@ -1,6 +1,8 @@
+from math import comb
 def countGoodArrays(self, n: int, m: int, k: int) -> int:
-    
-    
-    
-    
-    return
+    nums_chuck=n-k
+    nums_connect=comb(n-1,k)
+    one_connect_how_many_times=m*((m-1)**(nums_chuck-1))
+    allGoodArrays=nums_connect*one_connect_how_many_times
+    MOD=10**9+7
+    return allGoodArrays%MOD
