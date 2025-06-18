e9a7b73d88147a4f154f6ed7aeff333beed33dce
あやめさいこう <sean42593919@gmail.com>
2025-06-18 20:06:46 +0800

    modify code

--- leetcode/2966.divideArray.py
+++ leetcode/2966.divideArray.py
@@ -3,15 +3,11 @@ def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
     Nnums=[]
     if (Element%3)==0:
         nums.sort()
-        index,find=0,True
-        while((index/3)<(Element/3) and find):
-            if abs(nums[index]-nums[index+1])<=k and abs(nums[index+1]-nums[index+2])<=k and abs(nums[index]-nums[index+2])<=k:
+        index=0
+        while((index/3)<(Element/3)):
+            if abs(nums[index]-nums[index+2])<=k:
                 Nnums.append([nums[index],nums[index+1],nums[index+2]])
                 index+=3
             else:
-                find=False
-                break
-    if find==False:
-        return []
-    else:
-        return Nnums
+                return []
+    return Nnums
